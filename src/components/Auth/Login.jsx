b88ebe817a05a7bc151b9b5dbef478c332e6f3d7
odairorso <odairolimpiodeoliveira@gmail.com>
2025-07-28 09:33:04 -0400

    "fix"

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { supabase } from '../../services/supabaseClient';
+import { supabase } from '@services/supabaseClient';
 
 export default function Login() {
   const [email, setEmail] = useState('');
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@services': path.resolve(__dirname, './src/services'),
+    },
+  },
   server: {
     port: 3000,
     open: true
