4a24106313013d4981c48d3c2dcc287b5ca9e41c
odairorso <odairolimpiodeoliveira@gmail.com>
2025-07-28 10:19:35 -0400

    fix: corrige alias de path no vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@services': path.resolve(__dirname, './src/services'),
-    },
+      '@services': fileURLToPath(new URL('./src/services', import.meta.url))
+    }
   },
   server: {
     port: 3000,
